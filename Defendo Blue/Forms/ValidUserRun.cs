eae510cd0b8e5c28060d0cd22d3b0a2b0b059286
mertbilger <21430070035@mersin.edu.tr>
2024-08-28 16:44:34 +0300

    up

--- Defendo Blue/Forms/ValidUserRun.cs
+++ Defendo Blue/Forms/ValidUserRun.cs
@@ -10,29 +10,24 @@ namespace Defendo_Blue.Forms
         public ValidUserRun()
         {
             InitializeComponent();
-            this.Load += new EventHandler(AllUserRun_Load); // Form yüklendiğinde event handler'ı bağla
+            this.Load += new EventHandler(AllUserRun_Load); 
         }
 
         private void AllUserRun_Load(object sender, EventArgs e)
         {
             try
             {
-                // Geçerli kullanıcı adını al
                 string username = Environment.UserName;
 
-                // Kullanıcıya ait Startup dizininin yolunu oluştur
                 string path = $@"C:\Users\{username}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup";
 
-                // Dosyaların yolunu al
                 string[] files = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly);
 
-                // Dosya isimlerini al ve checkedListBox1'e ekle
                 checkedListBox1.Items.Clear(); // Mevcut öğeleri temizle
                 checkedListBox1.Items.AddRange(files.Select(file => Path.GetFileName(file)).ToArray());
             }
             catch (Exception ex)
             {
-                // Hata durumunda kullanıcıya bilgi ver
                 MessageBox.Show("Dosyalar alınırken bir hata oluştu: " + ex.Message);
             }
         }
