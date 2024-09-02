ee1aba75cf04bf30727a32b590ca7ff6a1b34048
Sinan Yücel <44983107+sinanyucel@users.noreply.github.com>
2024-09-02 12:11:05 +0300

    Update eventtypeenums.cs

---
+++ Defendo Blue/eventtypeenums.cs
@@ -0,0 +1,69 @@
+public enum EventList
+        {
+            [Description("Basarili giris")]
+            SuccesfulLogin = 4624,
+            [Description("Basarisiz giris")]
+            FailedLogin = 4625,
+            [Description("Ekran kilitlendi")]
+            Unlock = 4800,
+            [Description("Ekran kilidi acildi")]
+            Lock = 4801,
+            [Description("Kullanici oturumunu kapatti")]
+            Logoff1 = 4634,
+            [Description("Başarılı Logoff")]
+            Logoff2 = 4647,
+            [Description("Yeni kullanici olusturuldu")]
+            NewUserCreated = 4720,
+            [Description("Kullanici silindi")]
+            UserDeleted = 4726,
+            [Description("Loglar silindi")]
+            LogsCleared = 1102,
+            [Description("Kullanici aktif hale getirildi")]
+            AccountEnabled = 4722,
+            [Description("Bir kullanıcı hesabı değiştirildi")]
+            AccountChanged = 4738,
+            [Description("Bir hesap şifresini sıfırlama girişiminde bulunuldu")]
+            AccountPasswordChanged = 4724,
+            [Description("Bir kullanıcı hesabı devre dışı bırakıldı")]
+            AccountDisabled = 4725,
+            [Description("Bir ağ paylaşım nesnesine erişildi")]
+            AdministrativeShareAccess = 4725,
+            [Description("Windows Güvenlik Duvarı Hizmeti başarıyla başlatıldı")]
+            FirewallOn = 5024,
+            [Description("Windows Güvenlik Duvarı Hizmeti durduruldu")]
+            FirewallOff = 5025,
+            [Description("Sisteme bir servis kuruldu")]
+            NewWServiceCreated = 4697,
+            [Description("Bir ağ paylaşım nesnesine erişildi")]
+            AdministrativeShareAccess2 = 5140,
+            [Description("Remote disconnected: Bir oturumun bir Window.Station ile bağlantısı kesildi")]
+            RemoteDisconnected = 4779
+        }
+
+        public enum LoginTypes
+        {
+            [Description("System only")]
+            SystemOnly = 0,
+            [Description("Yerel login (Örn: Klavye ile)")]
+            YerelLogin = 2,
+            [Description("Network Login")]
+            NetworkLogin = 3,
+            [Description("Batch Login- zamanlanmış görevler için kullanılır")]
+            BatchLogin = 4,
+            [Description("Windows servis login – (Görünür/Interaktif olmayacaktır)")]
+            WinServiceLogin = 5,
+            [Description("Kilit ekranını açmak/kapatmak için kimlik bilgileri kullanıldı")]
+            KilitEkraniAcmakIcinKullanilanLoginBilgisi = 6,
+            [Description("Ağ üzerinden kimlik bilgileri clear text olarak gönderildi")]
+            AgdanGelenLoginBilgisi = 7,
+            [Description("Şu anki oturumu açan haricinde ”run as” komutuyla (Yönetici vs. olarak çalıştır ) yeni kimlik bilgileri kullanıldı")]
+            RunAsOlarakGelenLogin = 8,
+            [Description("RDP (Uzak Masaüstü)")]
+            RDP = 9,
+            [Description("Login kimliği cachten getirildi")]
+            CacheLogin = 10,
+            [Description("Cachten RDP yapıldı")]
+            CacheRDP = 11,
+            [Description("Cachten kilit açıldı (oturum zaten açık)")]
+            CacheKilidiAcildi = 12
+        }
