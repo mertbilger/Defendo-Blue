c1a3f6c76a5f6546af1c4e816c18548ec56dc4af
mertbilger <21430070035@mersin.edu.tr>
2024-09-06 12:41:38 +0300

    Refactor code

--- Defendo Blue/Versiyonla/Versiyonla.bat
+++ Defendo Blue/Versiyonla/Versiyonla.bat
@@ -5,39 +5,30 @@ setlocal enabledelayedexpansion
 set "source_folder=C:\Users\Mert\Desktop\Kaynak"
 set "target_folder=C:\Users\Mert\Desktop\Hedef"
 
-
 for /d /r "%target_folder%" %%d in (*) do (
     set "relative_path=%%d"
     set "relative_path=!relative_path:%target_folder%=!"
     set "source_folder_check=%source_folder%!relative_path!"
     
     if not exist "!source_folder_check!" (
-       
-        echo [INFO] Hedef klasor kaynakta bulunamadi, siliniyor: %%d
-        
+
+        echo [INFO] Hedef klasor kaynakta bulunamadi, siliniyor: %%d        
     )
 )
-
 
 for /r "%source_folder%" %%f in (*) do (
     
     for /f %%a in ('powershell -command "Get-FileHash -Algorithm SHA256 -Path \"%%f\" | Select-Object -ExpandProperty Hash"') do set "source_hash=%%a"
     
-   
     set "file_extension=%%~xf"
-
-    
     set "relative_path=%%f"
     set "relative_path=!relative_path:%source_folder%=!"
     set "target_file=%target_folder%!relative_path!"
-
     
     if not exist "!target_file!\.." mkdir "!target_file!\.."
-
     
     if exist "!target_file!" (
         for /f %%b in ('powershell -command "Get-FileHash -Algorithm SHA256 -Path \"!target_file!\" | Select-Object -ExpandProperty Hash"') do set "target_hash=%%b"
-
         
         if /i "!source_hash!" neq "!target_hash!" (
             
@@ -47,14 +38,13 @@ for /r "%source_folder%" %%f in (*) do (
             set "time_part=!time_part: =0!"
             set "timestamp=!date_part!_!time_part!"
             set "versioned_file=%target_folder%!relative_path!_!timestamp!!file_extension!"
-
-            
+     
             echo [INFO] Dosyalar degisti: !relative_path!, eski dosya versiyonlanacak.
-            copy "!target_file!" "!versioned_file!"
-            
-            
+            copy "!target_file!" "!versioned_file!"               
             copy "%%f" "!target_file!"
+
         ) else (
+
             echo [INFO] Dosya ayni: !relative_path!, kopyalamaya gerek yok.
         )
     ) else (
